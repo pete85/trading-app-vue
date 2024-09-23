9df6dc8123ee183a5952e90e319d0262a827428b
Piotr Furmanek <info.pete85@gmail.com>
2024-09-23 13:21:44 +0100

    base url

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const baseURL = mode === 'production'
-      ? 'https://wondrous-pie-16e693.netlify.app/'  // GitHub Pages expects the base URL to match the repository name
+      ? '/'  // GitHub Pages expects the base URL to match the repository name
       : '/';                 // Local development uses the root path
 
   return {
