e26cf06e0b6a3c91fd60f053f3b4d5ab2d96162b
Piotr Furmanek <info.pete85@gmail.com>
2024-09-23 12:49:56 +0100

    headers and vite config

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,20 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({ mode }) => {
+  const baseURL = mode === 'production'
+      ? 'https://pete85.github.io/trading-app-vue/'  // GitHub Pages expects the base URL to match the repository name
+      : '/';                 // Local development uses the root path
+
+  return {
+    base: baseURL,  // Use the baseURL variable directly, not a comparison
+    plugins: [
+      vue(),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
     }
-  }
-})
+  };
+});
