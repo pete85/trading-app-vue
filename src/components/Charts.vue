50a768b9908a7f0a0984db9a99b94578cd9d9476
Piotr Furmanek <info.pete85@gmail.com>
2024-09-23 22:27:32 +0100

    data fetched in 30 secs intervals, page divided into home and charts

---
+++ src/components/Charts.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps({
+  prices: {
+    type: Array,
+    required: true
+  }
+});
+</script>
+
+<template>
+  <div class="tw-flex tw-flex-auto tw-flex-col animated fadeIn">
+    <h2>Bitcoin Price Information</h2>
+
+    <ul>
+      <li v-for="(price, index) in prices" :key="index">
+        Time: {{ price.time.updated }} | USD: {{ price.bpi.USD.rate }} | GBP: {{ price.bpi.GBP.rate }} | EUR: {{ price.bpi.EUR.rate }}
+      </li>
+    </ul>
+  </div>
+</template>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,27 +1,19 @@
 <script setup>
 import { defineProps } from 'vue';
 
-// Define the props passed from the parent component
 const props = defineProps({
-  tradingData: Object
+  latestPrice: {
+    type: Object,
+    default: null
+  }
 });
 </script>
 
 <template>
-  <div class="tw-flex tw-flex-auto tw-flex-col animated fadeIn">
-    <h2>Bitcoin Price Information</h2>
-    <div v-if="tradingData">
-      <p>Chart Name: {{ tradingData.chartName }}</p>
-      <p>Updated Time: {{ tradingData.time.updated }}</p>
-      <h3>BPI (Bitcoin Price Index):</h3>
-      <ul>
-        <li v-for="(currency, key) in tradingData.bpi" :key="key">
-          {{ key }}: {{ currency.rate }} {{ currency.description }}
-        </li>
-      </ul>
-    </div>
-    <div v-else>
-      <p>Loading data...</p>
+  <div>
+    <h2>Latest Bitcoin Price</h2>
+    <div v-if="latestPrice">
+      Time: {{ latestPrice.time.updated }} | USD: {{ latestPrice.bpi.USD.rate }} | GBP: {{ latestPrice.bpi.GBP.rate }} | EUR: {{ latestPrice.bpi.EUR.rate }}
     </div>
   </div>
 </template>
